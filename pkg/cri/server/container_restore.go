a6832fe2971f10a6eca4e5c8cafb07e998f727d3
lin <451846939@qq.com>
2023-12-07 18:28:40 +0800

    add restore

--- pkg/cri/server/container_restore.go
+++ pkg/cri/server/container_restore.go
@@ -36,6 +36,7 @@ func (s *criService) checkIfCheckpointImage(ctx context.Context, input string) (
 		log.G(ctx).Errorf("Failed to get image status of %q: %v", input, err)
 		return false, err
 	}
+	log.G(ctx).Infof("Found checkpoint of container %v in %v", imageStatusRespone.Image, input)
 	if imageStatusRespone == nil ||
 		imageStatusRespone.Image == nil ||
 		imageStatusRespone.Image.Spec == nil ||
@@ -46,6 +47,7 @@ func (s *criService) checkIfCheckpointImage(ctx context.Context, input string) (
 
 	ann, ok := imageStatusRespone.Image.Spec.Annotations[annotations.CheckpointAnnotationName]
 	if !ok {
+		log.G(ctx).Infof("No checkpoint found CheckpointAnnotationName in %v", input)
 		return false, nil
 	}
 
