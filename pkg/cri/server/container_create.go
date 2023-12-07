73f0a7af62ec4a7f3f77e158bc05757766cc6cbc
lin <451846939@qq.com>
2023-12-07 11:00:49 +0800

    add restore

--- pkg/cri/server/container_create.go
+++ pkg/cri/server/container_create.go
@@ -78,6 +78,7 @@ func (c *criService) CreateContainer(ctx context.Context, r *runtime.CreateConta
 	log.G(ctx).Debugf("Container config %+v", config)
 	sandboxConfig := r.GetSandboxConfig()
 	var ctrId *string
+	log.L.Info("checkpointImage", checkpointImage)
 	if checkpointImage {
 		//todo restore
 		sandboxConfig, config, err = c.CRImportCheckpoint(ctx, config, sandboxConfig.GetMetadata().Uid, ctrId)
