029b5c682d5ace7d61a32e3b17ed9e03d34e8bab
lin <451846939@qq.com>
2023-12-12 14:28:34 +0800

    add restore

--- pkg/cri/server/image_status.go
+++ pkg/cri/server/image_status.go
@@ -56,11 +56,11 @@ func (c *criService) ImageStatus(ctx context.Context, r *runtime.ImageStatusRequ
 	if err != nil {
 		return nil, fmt.Errorf("failed to generate image info: %w", err)
 	}
-	var ptr *runtime.ImageSpec
+	ptr := &runtime.ImageSpec{}
+	//var ptr *runtime.ImageSpec
 	//ptr.Image = image.References
 	ptr.Annotations = image.Annotations
 	runtimeImage.Spec = ptr
-	//imageSpec := NewImageSpec(ptr)
 	return &runtime.ImageStatusResponse{
 		Image: runtimeImage,
 		Info:  info,
